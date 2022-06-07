adb17aa1a8fecdf7c5743347361571dfe76bdcfa
Nicole Steadman <nsteadman@gmail.com>
2022-06-07 11:44:41 -0700

    commit tree 53f22f5786603fedcd2527d2a95c4a19d4d1f73b

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,30 @@
+class CampersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        campers = Camper.all
+        render json: campers
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperActivitySerializer
+    end
+
+    def create
+        camper = Camper.create!(params.permit(:name, :age))
+        render json: camper, status: :created
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Camper not found" }, status: :not_found
+    end
+    
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+end
